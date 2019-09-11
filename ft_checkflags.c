daf2c707f1914e31303b953f99bb2f737b5d4a39
Tiffany MABUNDA <tmabunda@c5r7s6.wethinkcode.co.za>
2019-09-11 14:39:34 +0200

    OK

--- ft_checkflags.c
+++ ft_checkflags.c
@@ -6,11 +6,24 @@
 /*   By: tmabunda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 13:12:34 by tmabunda          #+#    #+#             */
-/*   Updated: 2019/09/11 13:24:11 by tmabunda         ###   ########.fr       */
+/*   Updated: 2019/09/11 14:39:24 by tmabunda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	checkflag(int argc, char **argv)
+char	checkflag(char **c)
 {
+	int i = 1;
 
+	while(c[i] != NULL)
+	{
+		if(c[i][0] != '-')
+			break;
+		if(c[i][0] == '-')
+			return(1);
+		i++;
+	}
+	return(0);
 }
+
+/* The purpose of this function is to check if theres - 
+ 
