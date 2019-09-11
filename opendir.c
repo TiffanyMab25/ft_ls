f6f72a1824ecab041bfcaa4418fb1f1e7d00cc2b
Tiffany MABUNDA <tmabunda@c5r7s6.wethinkcode.co.za>
2019-09-11 14:22:23 +0200

    11/09/2019

--- opendir.c
+++ opendir.c
@@ -6,7 +6,7 @@
 /*   By: tiffany_mab_ <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 14:57:19 by tiffany_m         #+#    #+#             */
-/*   Updated: 2019/09/11 13:46:39 by tmabunda         ###   ########.fr       */
+/*   Updated: 2019/09/11 14:22:00 by tmabunda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,22 @@
 
 void	ft_opendir(char	*path)
 {
-	DIR				*dir;
+	DIR				*filedir;
 	struct	dirent	*dir_to;
 
 	dir = opendir(path);
-	while ((dir_to = readdir(dir)) != NULL)
+	while ((dir_to = readdir(filedir)) != NULL)
 	{
 		printf("%s\n", dir_to->d_name);
 	}
-	closedir(dir);
+	closedir(filedir);
 }
+
+/* gcc -Wall -Werror -Wextra opendir.c main.c
+ * a.out .
+ * With that being done it should open the content within the current directory
+ * d_name is the size of the name within every directory [1024]
+ * While reading content within the while loop print it out
+ * Once one is done reading the directory it is always recommended that you close it just * like when one mallocs they always free their work */
 
 
